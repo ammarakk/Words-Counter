4ca7edacf108e84db3e6fd7e6cc78548edff3122
Ammar Ahmed Khan <161769688+ammarakk@users.noreply.github.com>
2024-04-18 02:41:48 +0500

    created

---
+++ index.js
@@ -0,0 +1,13 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const anwser = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "ENTER SENTENCE YOU WANT TO COUNT THE WORDS: "
+    }
+]);
+const words = anwser.sentence.trim().split(" ");
+console.log(words);
+console.log(chalk.blueBright(`Your Sentence Words Count Is: ${words.length}`));
---
+++ index.ts
@@ -0,0 +1,18 @@
+#! usr/bin/env node
+
+import inquirer from "inquirer"
+import chalk from "chalk"
+
+const anwser :{ sentence :string} = await inquirer.prompt([
+     { 
+       name: "sentence",
+       type :"input",
+       message : "ENTER SENTENCE YOU WANT TO COUNT THE WORDS: "
+     }
+])
+
+const words = anwser.sentence.trim().split(" ")
+
+console.log(words)
+
+console.log(chalk.blueBright(`Your Sentence Words Count Is: ${words.length}`))
